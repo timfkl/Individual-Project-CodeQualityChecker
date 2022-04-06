181abdf1fcabe2227af8d069e43931d0b191be4d
timfkl <timfkl11@gmail.com>
2022-04-06 13:32:35 +0100

    Working on CheckSpaces
    Might switch to using Regex

--- CodeQualityChecker/Tester.cs
+++ CodeQualityChecker/Tester.cs
@@ -3,22 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CodeQualityChecker
 {
     class Tester
     {
-        private List<string> fileList;
-        private List<string> FileList { get => fileList; set => fileList = value; }
+        
 
-        public Tester(List<string> fileList)
+        public Tester()
         {
-            FileList = fileList;
+            
         }
 
         public void InitializeDoxygen()
         {
 
+        }
+
+        public void CheckSpaces(List<string> fileList)
+        {
+            string[] textFileContent;
+            foreach(string file in fileList)
+            {
+                textFileContent = File.ReadAllLines(file, Encoding.UTF8);
+                CheckSpacesHelper(textFileContent);
+            }
+        }
+
+        public List<int> CheckSpacesHelper(string[] fileContent)
+        {
+            List<int> indentList = new List<int>;
+            int count = 0;
+            foreach( string line in fileContent)
+            {
+                count = line.TakeWhile(Char.IsWhiteSpace).Count();
+                indentList.Add(count);
+            }
+            return indentList;
         }
     }
 }
